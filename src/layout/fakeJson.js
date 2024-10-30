9f02df79071d8ec509c70ddf0a02f9b1daec772b
Dilshod <dilshodbunyodov2020@gmail.com>
2024-10-30 17:05:34 +0500

    map

---
+++ src/layout/fakeJson.js
@@ -0,0 +1,22 @@
+export const Menu = [
+    {
+        img: "./car.svg",
+        title: 'Yuk tashish'
+    },
+    {
+        img: "./car.svg",
+        title: 'Yo\'lovchilarni tashish'
+    },
+    {
+        img: "./car.svg",
+        title: 'Maxsus texnika xizmatlari'
+    },
+    {
+        img: "./car.svg",
+        title: 'Transport ijarasi'
+    },
+    {
+        img: "./car.svg",
+        title: 'Transport transferi'
+    },
+]
